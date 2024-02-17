bf278923775c2cd530fedac94302ac88b7dbe9b3
Nilsolk <awer258@gmail.com>
2024-02-17 10:22:53 +0300

    made main_screen layout and location_screen layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.11.0")
     val room_version = "2.6.1"
     //Retrofit2
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
